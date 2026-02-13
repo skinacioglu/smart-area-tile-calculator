8f2426619e9723c912f21f7c40ad66bb24837c1f
skinacioglu <serkankinacioglu@gmail.com>
2026-02-13 19:42:47 +0300

    feat: menu

--- app/routes/app.additional.tsx
+++ app/routes/app.additional.tsx
@@ -1,6 +1,6 @@
 export default function AdditionalPage() {
   return (
-    <s-page heading="Support & Contact">
+    <s-page heading="Support & Contact-2">
       <s-section heading="Multiple pages">
         <s-paragraph>
           The app template comes with an additional page which demonstrates how
