790bf55cf73836134b7ad635f4e530f1ceadf671
skinacioglu <serkankinacioglu@gmail.com>
2026-02-13 19:37:57 +0300

    feat: update navigation menu and sync offline access token

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -106,7 +106,7 @@ export default function Index() {
         Generate a product
       </s-button>
 
-      <s-section heading="Congrats on creating a new Shopify app 🎉">
+      <s-section heading="Congrats on creating a new Shopify app -111222 🎉">
         <s-paragraph>
           This embedded app template uses{" "}
           <s-link
