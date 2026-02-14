a6bbdc150ad662088f8ab97d4c12f6cb28a386d7
skinacioglu <serkankinacioglu@gmail.com>
2026-02-14 12:35:43 +0300

    feat: app menu updated

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -18,7 +18,7 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app">User Guide</s-link>
+        <s-link href="/app">Overview</s-link>
         <s-link href="/app/additional">Support & Contact</s-link>
         <s-link href="/app/advanced-setup-guide">Advanced Setup Guide</s-link>
       </s-app-nav>
